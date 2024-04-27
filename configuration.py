3938c931085f444cf2155e6e091764c3cfe5cc99
ValerianoH <167591009+ValerianoH@users.noreply.github.com>
2024-04-27 16:46:44 -0400

    URL actualizada

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-d33cb9de-66b5-4cb5-bd15-a95ae42676f1.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-9f48148a-80fa-4152-a1dd-acd4f87e70c2.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
